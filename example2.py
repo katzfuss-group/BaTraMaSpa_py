ea346d4001c6a020b50af4e3bdfb581ac3934f6f
Jian Cao <jcao2416@gmail.com>
2022-03-22 10:25:20 -0500

    Add timer to example2

--- example2.py
+++ example2.py
@@ -26,6 +26,7 @@ So torch here is imported after NN array is constructed
 """
 
 import torch
+import time
 from fit_map import fit_map_mini, compute_scal, cond_samp
 
 
@@ -36,46 +37,49 @@ NN = torch.from_numpy(NN)[:, 1:]
 
 
 scal = compute_scal(locs, NN)
-fitLin = fit_map_mini(data, NN, linear=True, scal=scal, lr=1e-4, maxIter=100)
-fitNonlin = fit_map_mini(data, NN, linear=False, scal=scal, lr=1e-4, maxIter=100)
+# fitLin = fit_map_mini(data, NN, linear=True, lr=1e-4)
+tic = time.perf_counter()
+fitNonlin = fit_map_mini(data, NN, linear=False, lr=1e-4)
+toc = time.perf_counter()
+print(f"fit_map used {toc - tic:0.4f} seconds")
 
-
-i = 79
-NNrow = NN[i, :]
-xFix = torch.zeros(i)
-xFix[NNrow] = data[:, NNrow].mean(dim=0)
-nVal = 20
-NNVal = torch.zeros(2, nVal)
-NNVal[0, :] = torch.linspace(start=data[:, NNrow[0]].min(dim=0).values,
-                             end=data[:, NNrow[0]].max(dim=0).values,
-                             steps=nVal)
-NNVal[1, :] = torch.linspace(start=data[:, NNrow[1]].min(dim=0).values,
-                             end=data[:, NNrow[1]].max(dim=0).values,
-                             steps=nVal)
-fx = torch.zeros(nVal, nVal)
-fxLin = torch.zeros(nVal, nVal)
-with torch.no_grad():
-    for k in range(nVal):
-        for l in range(nVal):
-            xFix[NNrow[:2]] = torch.tensor([NNVal[0, k], NNVal[1, l]])
-            fx[k, l] = cond_samp(fitNonlin, 'fx', xFix=xFix, indLast=i)[i]
-            fxLin[k, l] = cond_samp(fitLin, 'fx', xFix=xFix, indLast=i)[i]
-
-
-import matplotlib.pyplot as plt
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-X, Y = np.meshgrid(NNVal[0, :], NNVal[1, :])
-ax.scatter3D(X, Y, fxLin, c=fxLin, cmap='Greens')
-ax.set_xlabel('1st NN')
-ax.set_ylabel('2nd NN')
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-X, Y = np.meshgrid(NNVal[0, :], NNVal[1, :])
-ax.scatter3D(X, Y, fx, c=fx, cmap='Greens')
-ax.set_xlabel('1st NN')
-ax.set_ylabel('2nd NN')
+#
+# i = 79
+# NNrow = NN[i, :]
+# xFix = torch.zeros(i)
+# xFix[NNrow] = data[:, NNrow].mean(dim=0)
+# nVal = 20
+# NNVal = torch.zeros(2, nVal)
+# NNVal[0, :] = torch.linspace(start=data[:, NNrow[0]].min(dim=0).values,
+#                              end=data[:, NNrow[0]].max(dim=0).values,
+#                              steps=nVal)
+# NNVal[1, :] = torch.linspace(start=data[:, NNrow[1]].min(dim=0).values,
+#                              end=data[:, NNrow[1]].max(dim=0).values,
+#                              steps=nVal)
+# fx = torch.zeros(nVal, nVal)
+# fxLin = torch.zeros(nVal, nVal)
+# with torch.no_grad():
+#     for k in range(nVal):
+#         for l in range(nVal):
+#             xFix[NNrow[:2]] = torch.tensor([NNVal[0, k], NNVal[1, l]])
+#             fx[k, l] = cond_samp(fitNonlin, 'fx', xFix=xFix, indLast=i)[i]
+#             fxLin[k, l] = cond_samp(fitLin, 'fx', xFix=xFix, indLast=i)[i]
+#
+#
+# import matplotlib.pyplot as plt
+#
+#
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# X, Y = np.meshgrid(NNVal[0, :], NNVal[1, :])
+# ax.scatter3D(X, Y, fxLin, c=fxLin, cmap='Greens')
+# ax.set_xlabel('1st NN')
+# ax.set_ylabel('2nd NN')
+#
+#
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# X, Y = np.meshgrid(NNVal[0, :], NNVal[1, :])
+# ax.scatter3D(X, Y, fx, c=fx, cmap='Greens')
+# ax.set_xlabel('1st NN')
+# ax.set_ylabel('2nd NN')
