94f289fc9c38a9da39c05d3d183522e105add3dc
Daniel J. Drennan <danjdrennan@gmail.com>
2022-08-02 00:00:40 -0500

    coerces long int type for NN array
    Allowing floats causes an error when subsetting on Windows. Coercing
    the data type of the array to long integers enables subsetting, which
    should clear issues in a Windows environment.

--- NNarray.py
+++ NNarray.py
@@ -4,9 +4,9 @@ import warnings
 from scipy.spatial.distance import cdist
 
 
-def NN_L2(locs: 'numpy.ndarray', m: 'int') -> 'numpy.ndarray':
+def NN_L2(locs: "numpy.ndarray", m: "int") -> "numpy.ndarray":
     n, d = locs.shape
-    NN = - np.ones((n, m + 1), dtype=int)
+    NN = -np.ones((n, m + 1), dtype=int)
     mult = 2
     maxVal = min(m * mult + 1, n)
     distM = cdist(locs[:maxVal, :], locs[:maxVal, :])
@@ -18,23 +18,23 @@ def NN_L2(locs: 'numpy.ndarray', m: 'int') -> 'numpy.ndarray':
         NN[i, :NNlen] = NNrow[:NNlen]
     queryIdx = np.arange(maxVal, n)
     mSearch = m
-    while (queryIdx.size > 0):
+    while queryIdx.size > 0:
         maxIdx = queryIdx.max()
-        mSearch = min(maxIdx+1, 2 * mSearch)
+        mSearch = min(maxIdx + 1, 2 * mSearch)
         if n < 1e5:
             index = faiss.IndexFlatL2(d)
         else:
             quantizer = faiss.IndexFlatL2(d)
-            index = faiss.IndexIVFFlat(quantizer, d, min(maxIdx+1, 1024))
-            index.train(locs[:maxIdx+1, :])
-            index.nprobe = min(maxIdx+1, 256)
-        index.add(locs[:maxIdx+1, :])
+            index = faiss.IndexIVFFlat(quantizer, d, min(maxIdx + 1, 1024))
+            index.train(locs[: maxIdx + 1, :])
+            index.nprobe = min(maxIdx + 1, 256)
+        index.add(locs[: maxIdx + 1, :])
         _, NNsub = index.search(locs[queryIdx, :], int(mSearch))
         lessThanI = NNsub <= queryIdx[:, None]
         numLessThanI = lessThanI.sum(1)
         idxLessThanI = np.nonzero(np.greater_equal(numLessThanI, m + 1))[0]
         for i in idxLessThanI:
-            NN[queryIdx[i]] = NNsub[i, lessThanI[i, :]][:m+1]
+            NN[queryIdx[i]] = NNsub[i, lessThanI[i, :]][: m + 1]
             if NN[queryIdx[i], 0] != queryIdx[i]:
                 try:
                     idx = np.nonzero(NN[queryIdx[i]] == queryIdx[i])[0][0]
@@ -45,5 +45,4 @@ def NN_L2(locs: 'numpy.ndarray', m: 'int') -> 'numpy.ndarray':
         queryIdx = np.delete(queryIdx, idxLessThanI, 0)
     if np.any(NN[:, 0] != np.arange(n)):
         warnings.warn("There are very close locations and NN[:, 0] != np.arange(n)\n")
-    return NN
-
+    return NN.long()
