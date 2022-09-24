3aa4f829e4d26aeb2ee285c6b8ef3727e965f543
Daniel J. Drennan <danjdrennan@gmail.com>
2022-09-24 00:23:58 -0500

    enables importing package from other dirs, simplifies env setup
    This commit provides an __init__ file so we can import the package
    in other directories. The Python sys library allows to provide a
    path to this repo locally for import.
    
    It also furnishes a yaml file that can simplify environment setup
    using miniconda3 or the anaconda package manager (the same thing).
    
    Contact me <danjdrennan> for assistance with environtment setup or
    importing parts of this package from other directories. This change
    makes working in a Jupyter notebook, for example, a lot easier.

--- NNarray.py
+++ NNarray.py
@@ -1,49 +1,49 @@
-import numpy as np
-import faiss
 import warnings
+from numpy import ndarray, any, argsort, arange, delete, greater_equal, int32, int64, nonzero, ones
 from scipy.spatial.distance import cdist
 
+import faiss
 
-def NN_L2(locs: 'numpy.ndarray', m: 'int') -> 'numpy.ndarray':
+
+def NN_L2(locs: ndarray, m: int) -> ndarray:
     n, d = locs.shape
-    NN = - np.ones((n, m + 1), dtype=int)
+    NN = -ones((n, m + 1), dtype=int)
     mult = 2
     maxVal = min(m * mult + 1, n)
     distM = cdist(locs[:maxVal, :], locs[:maxVal, :])
-    odrM = np.argsort(distM)
+    odrM = argsort(distM)
     for i in range(maxVal):
         NNrow = odrM[i, :]
         NNrow = NNrow[NNrow <= i]
         NNlen = min(NNrow.shape[0], m + 1)
         NN[i, :NNlen] = NNrow[:NNlen]
-    queryIdx = np.arange(maxVal, n)
+    queryIdx = arange(maxVal, n)
     mSearch = m
-    while (queryIdx.size > 0):
+    while queryIdx.size > 0:
         maxIdx = queryIdx.max()
-        mSearch = min(maxIdx+1, 2 * mSearch)
+        mSearch = min(maxIdx + 1, 2 * mSearch)
         if n < 1e5:
             index = faiss.IndexFlatL2(d)
         else:
             quantizer = faiss.IndexFlatL2(d)
-            index = faiss.IndexIVFFlat(quantizer, d, min(maxIdx+1, 1024))
-            index.train(locs[:maxIdx+1, :])
-            index.nprobe = min(maxIdx+1, 256)
-        index.add(locs[:maxIdx+1, :])
+            index = faiss.IndexIVFFlat(quantizer, d, min(maxIdx + 1, 1024))
+            index.train(locs[: maxIdx + 1, :])
+            index.nprobe = min(maxIdx + 1, 256)
+        index.add(locs[: maxIdx + 1, :])
         _, NNsub = index.search(locs[queryIdx, :], int(mSearch))
         lessThanI = NNsub <= queryIdx[:, None]
         numLessThanI = lessThanI.sum(1)
-        idxLessThanI = np.nonzero(np.greater_equal(numLessThanI, m + 1))[0]
+        idxLessThanI = nonzero(greater_equal(numLessThanI, m + 1))[0]
         for i in idxLessThanI:
-            NN[queryIdx[i]] = NNsub[i, lessThanI[i, :]][:m+1]
+            NN[queryIdx[i]] = NNsub[i, lessThanI[i, :]][: m + 1]
             if NN[queryIdx[i], 0] != queryIdx[i]:
                 try:
-                    idx = np.nonzero(NN[queryIdx[i]] == queryIdx[i])[0][0]
+                    idx = nonzero(NN[queryIdx[i]] == queryIdx[i])[0][0]
                     NN[queryIdx[i], idx] = NN[queryIdx[i], 0]
                     NN[queryIdx[i], 0] = queryIdx[i]
                 except IndexError as inst:
                     NN[queryIdx[i], 0] = queryIdx[i]
-        queryIdx = np.delete(queryIdx, idxLessThanI, 0)
-    if np.any(NN[:, 0] != np.arange(n)):
+        queryIdx = delete(queryIdx, idxLessThanI, 0)
+    if any(NN[:, 0] != arange(n)):
         warnings.warn("There are very close locations and NN[:, 0] != np.arange(n)\n")
-    return NN
-
+    return NN.astype(int64)
